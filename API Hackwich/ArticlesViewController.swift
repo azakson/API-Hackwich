a8fb6148024dfd3c82e016a9c0c074133b972add
Avery <Zakson>
2020-01-31 10:43:24 -0600

    Finished initial set up of SourcesTableView

---
+++ API Hackwich/ArticlesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ArticlesViewController.swift
+//  API Hackwich
+//
+//  Created by Avery Zakson on 1/31/20.
+//  Copyright © 2020 mojo. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UITableViewController {
+
+}
